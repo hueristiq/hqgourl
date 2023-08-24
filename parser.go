65126bf448b6cb46a66c0238e944a6d5c0af3a38
Alex Munene (@enenumxela) <62714471+enenumxela@users.noreply.github.com>
2023-08-24 22:45:32 +0300

    chore: -

--- parser.go
+++ parser.go
@@ -9,39 +9,6 @@ import (
 	"golang.org/x/net/publicsuffix"
 )
 
-// URL represents a parsed URL (technically, a URI reference).
-//
-// The general form represented is:
-//
-//	[scheme:][//[userinfo@]host][/]path[?query][#fragment]
-//
-// URLs that do not start with a slash after the scheme are interpreted as:
-//
-//	scheme:opaque[?query][#fragment]
-//
-// https://sub.example.com:8080/path/to/file.txt
-type URL struct {
-	*url.URL
-	// Scheme      string    // e.g https
-	// Opaque      string    // encoded opaque data
-	// User        *Userinfo // username and password information
-	// Host        string    // e.g. sub.example.com, sub.example.com:8080
-	// Path        string    // path (relative paths may omit leading slash) e.g /path/to/file.txt
-	// RawPath     string    // encoded path hint (see EscapedPath method)
-	// OmitHost    bool      // do not emit empty host (authority)
-	// ForceQuery  bool      // append a query ('?') even if RawQuery is empty
-	// RawQuery    string    // encoded query values, without '?'
-	// Fragment    string    // fragment for references, without '#'
-	// RawFragment string    // encoded fragment hint (see EscapedFragment method)
-	Domain      string // e.g. sub.example.com
-	ETLDPlusOne string // e.g. example.com
-	Subdomain   string // e.g. sub
-	RootDomain  string // e.g. example
-	TLD         string // e.g. com
-	Port        string // e.g. 8080
-	Extension   string // e.g. .txt
-}
-
 // Parse parses a raw url into a URL structure.
 //
 // It uses the  `net/url`'s Parse() internally, but it slightly changes its behavior:
@@ -69,9 +36,13 @@ func ParseWithDefaultScheme(rawURL, defaultScheme string) (parsedURL *URL, err e
 		return
 	}
 
+	// 1. Original
+	parsedURL.Original = rawURL
+
+	// 2. Domains, 3. Ports
 	parsedURL.Domain, parsedURL.Port = SplitHost(parsedURL.URL.Host)
 
-	// ETLDPlusOne - Extract ETLDPlusOne
+	// 4. ETLDPlusOne - Extract ETLDPlusOne
 	parsedURL.ETLDPlusOne, err = publicsuffix.EffectiveTLDPlusOne(parsedURL.Domain)
 	if err != nil {
 		err = fmt.Errorf("[hqgoutils/url] %w", err)
@@ -79,15 +50,15 @@ func ParseWithDefaultScheme(rawURL, defaultScheme string) (parsedURL *URL, err e
 		return
 	}
 
-	// RootDomain + TLD - Determine the RootDomain and TLD
+	// 5. RootDomain, 6. TLD - Determine the RootDomain and TLD
 	parsedURL.RootDomain, parsedURL.TLD = splitETLDPlusOne(parsedURL.ETLDPlusOne)
 
-	// Subdomain - Determine the Subdomain, if any
+	// 7. Subdomain - Determine the Subdomain, if any
 	if subdomain := strings.TrimSuffix(parsedURL.Domain, "."+parsedURL.ETLDPlusOne); subdomain != parsedURL.Domain {
 		parsedURL.Subdomain = subdomain
 	}
 
-	// Extension
+	// 8. Extension
 	parsedURL.Extension = path.Ext(parsedURL.Path)
 
 	return
